33b85154cc1c213ed08104f9dfedd09afaf217cf
earaj <118749857+earaj@users.noreply.github.com>
2024-02-28 08:46:14 -0500

    Ajout de script d'insertion des produits

---
+++ Donnee/InsertionDonnees.sql
@@ -0,0 +1,30 @@
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (01,1,'aaa','torquent per conubia nostra, per inceptos hymenaeos.',300,200);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (02,12,'bbb','torquent per conubia nostra, per inceptos hymenaeos.',2500,20);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (03,13,'qqq','torquent per conubia nostra, per inceptos hymenaeos.',1300,300);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (04,14,'www','torquent per conubia nostra, per inceptos hymenaeos.',350,1200);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (05,15,'eee','torquent per conubia nostra, per inceptos hymenaeos.',30,2);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (06,16,'rrr','torquent per conubia nostra, per inceptos hymenaeos.',600,100);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (07,17,'ttt','torquent per conubia nostra, per inceptos hymenaeos.',100,200);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (08,18,'yyy','torquent per conubia nostra, per inceptos hymenaeos.',3600,7);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (09,19,'uuu','torquent per conubia nostra, per inceptos hymenaeos.',10,1555);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (10,111,'iii','torquent per conubia nostra, per inceptos hymenaeos.',550,70);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (11,112,'ooo','torquent per conubia nostra, per inceptos hymenaeos.',2,5569);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (12,113,'ppp','torquent per conubia nostra, per inceptos hymenaeos.',657,10);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (13,114,'nnn','torquent per conubia nostra, per inceptos hymenaeos.',999,1);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (14,115,'mmm','torquent per conubia nostra, per inceptos hymenaeos.',99,160);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
+VALUES (15,116,'kkk','torquent per conubia nostra, per inceptos hymenaeos.',159,215);
