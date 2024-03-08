3e91a8cc1ab7b2c1e89187f47592e29dc6900c8b
earaj <118749857+earaj@users.noreply.github.com>
2024-03-07 22:32:50 -0500

    Modification de script d'insertion en respectant le nouveau modele logique

--- Script/InsertionDonnees.sql
+++ Script/InsertionDonnees.sql
@@ -1,30 +1,38 @@
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (01,1,'aaa','torquent per conubia nostra, per inceptos hymenaeos.',300,200);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (02,12,'bbb','torquent per conubia nostra, per inceptos hymenaeos.',2500,20);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (03,13,'qqq','torquent per conubia nostra, per inceptos hymenaeos.',1300,300);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (04,14,'www','torquent per conubia nostra, per inceptos hymenaeos.',350,1200);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (05,15,'eee','torquent per conubia nostra, per inceptos hymenaeos.',30,2);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (06,16,'rrr','torquent per conubia nostra, per inceptos hymenaeos.',600,100);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (07,17,'ttt','torquent per conubia nostra, per inceptos hymenaeos.',100,200);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (08,18,'yyy','torquent per conubia nostra, per inceptos hymenaeos.',3600,7);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (09,19,'uuu','torquent per conubia nostra, per inceptos hymenaeos.',10,1555);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (10,111,'iii','torquent per conubia nostra, per inceptos hymenaeos.',550,70);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (11,112,'ooo','torquent per conubia nostra, per inceptos hymenaeos.',2,5569);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (12,113,'ppp','torquent per conubia nostra, per inceptos hymenaeos.',657,10);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (13,114,'nnn','torquent per conubia nostra, per inceptos hymenaeos.',999,1);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (14,115,'mmm','torquent per conubia nostra, per inceptos hymenaeos.',99,160);
-INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible)
-VALUES (15,116,'kkk','torquent per conubia nostra, per inceptos hymenaeos.',159,215);
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (02,12,'Laptop Gaming XYZ','torquent per conubia nostra, per inceptos hymenaeos.',2500,20,'Bleu','25cm x 15cm x 35cm','core i5','RTX 3070','16gb','1to',"Windows 10","15p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (03,13,'PC Portable ABC','torquent per conubia nostra, per inceptos hymenaeos.',1300,300,'Noir','28cm x 18cm x 38cm','AMD Ryzen 7','GTX 1660','12gb','512go',"Linux Ubuntu","14p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (04,14,'Desktop XYZ','torquent per conubia nostra, per inceptos hymenaeos.',350,1200,'Gris','32cm x 22cm x 42cm','Intel Core i7','Intel HD Graphics','16gb','256go',"Windows 11","17p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (05,15,'Tablet ABC','torquent per conubia nostra, per inceptos hymenaeos.',30,2,'Vert','20cm x 10cm x 25cm','AMD Athlon','N/A','4gb','128go',"MacOS","13p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (06,16,'Mini PC XYZ','torquent per conubia nostra, per inceptos hymenaeos.',600,100,'Jaune','27cm x 17cm x 37cm','Raspberry Pi 4','N/A','8gb','64go',"Raspbian","7p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (07,17,'Smartphone ABC','torquent per conubia nostra, per inceptos hymenaeos.',100,200,'Blanc','30cm x 20cm x 40cm','ARM Cortex','N/A','2gb','32go',"Android","10p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (08,18,'Serveur XYZ','torquent per conubia nostra, per inceptos hymenaeos.',3600,7,'Orange','35cm x 25cm x 45cm','Intel Xeon','Nvidia Quadro','64gb','2to',"Windows Server","21p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (09,19,'Micro PC ABC','torquent per conubia nostra, per inceptos hymenaeos.',10,1555,'Violet','22cm x 12cm x 32cm','ARM Cortex','N/A','1gb','16go',"Linux Debian","9p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (10,111,'All-in-One XYZ','torquent per conubia nostra, per inceptos hymenaeos.',550,70,'Marron','26cm x 16cm x 36cm','Intel Core i9','Nvidia GeForce GTX','32gb','4to',"Windows 10","19p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (11,112,'Smartwatch ABC','torquent per conubia nostra, per inceptos hymenaeos.',2,5569,'Beige','40cm x 30cm x 50cm','ARM Cortex','N/A','512mb','8go',"Android","8p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (12,113,'Portable XYZ','torquent per conubia nostra, per inceptos hymenaeos.',657,10,'Turquoise','24cm x 14cm x 34cm','Intel Pentium','N/A','6gb','128go',"Windows 7","16p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (13,114,'Desktop ABC','torquent per conubia nostra, per inceptos hymenaeos.',999,1,'Argent','29cm x 19cm x 39cm','AMD Ryzen 5','AMD Radeon','24gb','2to',"Windows 11","20p");
+
+INSERT INTO mybd.produit (id_produit,id_commande,nom_produit,description_produit,prix_unitaire,stock_disponible,couleur,dimensions,cpu,gpu,ram,stockage,Systeme_dexploitation,taille_ecran)
+VALUES (14,115,'Laptop XYZ','torquent per conubia nostra, per inceptos hymenaeos.',99,160,'Rose','33cm x 23cm x 43cm','AMD Ryzen 3','AMD Radeon RX','16gb','1to',"Windows 10","22p");
